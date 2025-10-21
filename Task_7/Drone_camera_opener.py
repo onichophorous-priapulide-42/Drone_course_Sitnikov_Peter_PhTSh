90f81724d73b1d4dd258f7bd4378db101da169f3
Sitandr <andr-sitnikov@mail.ru>
2025-10-21 18:59:29 +0300

    Made debug changes in camera

--- Task_7/Drone_camera_opener.py
+++ Task_7/Drone_camera_opener.py
@@ -1,12 +1,11 @@
 import cv2
 import numpy as np
-from pioneer_sdk import Pioneer
 from pioneer_sdk import Camera
-import time
 
-drone = Pioneer()
 cam = Camera()
-cam.connect()
-img = cam.get_cv_frame()
-if img is not None:
-    imshow("Drone camera", img)
+
+while True:
+    img = cam.get_cv_frame()
+    if img is not None:
+        cv2.imshow("Drone camera", img)
+    cv2.waitKey(1)
--- Task_7/Image_opener.py
+++ Task_7/Image_opener.py
@@ -1,3 +1,4 @@
 import cv2
-img = cv2.imread("C:/Drones_course/Graph_dop_vel.PNG")
+img = cv2.imread("C:/Drones_course/Task_7/Graph_dop_vel.PNG")
+print(img)
 cv2.imshow("Image Window", img)
